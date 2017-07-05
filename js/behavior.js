bfb9405e476943fce3d76a3a6cb8b15133d2d576
kramardmytro <kramardmytro@i.ua>
2017-07-05 23:24:42 +0300

    Markup changes and animation rewriting

--- js/behavior.js
+++ js/behavior.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  // Set height for animation container
+  var gradientHeignt = $('#gradient').height();
+  $('#animation').css({height: gradientHeignt});
+  $('#instructions').css({top: gradientHeignt});
+  $(window).resize(function() {
+    var resizeHeignt = $('#gradient').height();
+    $('#animation').css({height: resizeHeignt});
+    $('#instructions').css({top: resizeHeignt});
+  });
   // Disable Bootstrap on Enter form sending
   $('#answer').keydown(function(event) {
     if(event.keyCode == 13) {
@@ -11,37 +20,54 @@ $(document).ready(function() {
     var password = 'test';
     password = password.toUpperCase();
     var inputValue = $('#answer').val().toUpperCase();
+
     if ( password === inputValue) {
-      var animFlash = 'animated flash';
-      var animEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-      document.getElementById('audio').play();
-      $('div.checkpw-content').fadeOut('fast');
-      $('#gradient').removeClass('gradient').addClass('pixelBg');
-      $('#gradient').addClass(animFlash).one(animEnd, function(){
-        $(this).removeClass(animFlash);
+      var bounceIn = 'animated bounceIn';
+      var bounceOut = 'animated bounceOutUp';
+      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+      $('div.checkpw-content').addClass('animated fadeOutUpBig').delay(2000).queue(function() {
+        $(this).removeClass('animated fadeOutUpBig');
       });
-          
-      function successMessage() {
-        var animFadeDown = 'animated fadeInDownBig';
-        $('div.success-message').fadeIn();
-        $('#sm1').fadeIn(0).addClass(animFadeDown).delay(2500).fadeOut('slow');
-        $('#sm2').delay(3000).fadeIn(0).addClass(animFadeDown).delay(2500).fadeOut('slow');
-        $('#sm3').delay(6000).fadeIn(0).addClass(animFadeDown).delay(2500).fadeOut('slow');
-        $('#sm4').delay(9000).fadeIn(0).addClass(animFadeDown).delay(2500).fadeOut('slow');  
-        $('div.success-message').delay(12000).fadeOut(0);
-      };
-      setTimeout(successMessage, 2500);
+
+      document.getElementById('audio').play();
+
+      $('#gradient').delay(750).fadeOut('slow', function() {
+        $('#animation').fadeIn().addClass('animated flash').delay(2000).queue(function() {
+          $(this).removeClass('animated flash');
+        });
+      });
+
+      $('div.success-message').delay(2000).fadeIn(0);
+
+      $('#sm1').delay(2500).fadeIn(0).addClass(bounceIn).delay(2500).queue(function() {
+        $(this).removeClass(bounceIn).addClass(bounceOut);
+      });
+
+      $('#sm2').delay(6000).fadeIn(0).addClass(bounceIn).delay(2500).queue(function() {
+        $(this).removeClass(bounceIn).addClass(bounceOut);
+      });
+
+      $('#sm3').delay(9500).fadeIn(0).addClass(bounceIn).delay(2500).queue(function() {
+        $(this).removeClass(bounceIn).addClass(bounceOut);
+      });
 
       $('#answer').val('');
-      $('#dance').delay(15000).fadeIn(0).addClass('animated fadeInUpBig');
-      $('#dance').delay(23500).fadeOut(0);
-      
-      function pixelBg() {
-        $('#gradient').removeClass('pixelBg').addClass('gradient');
-        };
-      setTimeout(pixelBg, 39000);
-      
-      $('div.checkpw-content').delay(39500).fadeIn('slow');
+
+      $('#dance').delay(13000).fadeIn(0).addClass('animated bounceInDown');
+
+      $('#dance').delay(27000).fadeOut('slow').delay(1000).queue(function() {
+        $(this).removeClass('animated bounceInDown');
+      });
+
+      $('div.success-message').delay(40000).fadeOut(0);
+
+      //$('#animation').delay(40000).fadeOut('slow', function() {
+        //$('#gradient').fadeIn('slow');
+      //});
+
+      setTimeout(location.reload.bind(location), 42000);
+
       } else {
         $('#error').removeClass('error').addClass('error-out');
         $('#answer').focus(function(){
