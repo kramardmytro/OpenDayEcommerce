b93f3f11e7c3ca2bf7c6ebbcea3cbf26caf7c810
Dmytro Kramar/KIV/UA <dmytro.kramar@avon.com>
2017-07-21 12:31:33 +0300

    prefinal changes

--- js/behavior.js
+++ js/behavior.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
   });
   // Password checking and animated section
   function submitting(){
-    var password = '123';
+    var password = '9';
     password = password.toUpperCase();
     var inputValue = $('#answer').val().toUpperCase();
 
